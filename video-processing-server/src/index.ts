1c4ad1d10eb04449d9681e4631de1adc26e8378e
do2523 <do262431@gmail.com>
2024-07-25 15:51:38 -0400

    Setting up Processing-Video Logic

--- video-processing-server/src/index.ts
+++ video-processing-server/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";   // <symbolic link> Issue installing the actual ffmpeg  ***
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) =>{
-    res.send("Testing ...")
+app.post("/process-video", (req, res) =>{
+    // Get path of the input video file from the request body
+
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath){
+        res.status(400).send("Bad Request: Missing Input File Path")
+    }
+    if (!outputFilePath){
+        res.status(400).send("Bad Request: Missing Output File Path")
+    }
 })
 
 app.listen(port, () =>{

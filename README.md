6c5d14638bcf9315be7456c511067709c8848eb3
do2523 <do262431@gmail.com>
2024-07-26 17:00:21 -0400

    Set ffmpeg logic and set video processing to 360p

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # VideoApp
-# ---Testing File Transfers from Windows to Linux file system root directory-----
+# ---Currently working on: Docker file-----
--- video-processing-server/src/index.ts
+++ video-processing-server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import ffmpeg from "fluent-ffmpeg";   // <symbolic link> Issue installing the actual ffmpeg  ***
 
 const app = express();
-const port = 3000;
+app.use(express.json()); // So express can handle JS req
 
 app.post("/process-video", (req, res) =>{
     // Get path of the input video file from the request body
@@ -16,8 +16,22 @@ app.post("/process-video", (req, res) =>{
     if (!outputFilePath){
         res.status(400).send("Bad Request: Missing Output File Path")
     }
+
+    ffmpeg(inputFilePath) 
+        .outputOptions("-vf", "scale = -1:360") //Resolution to 360p
+        .on("end", () => {
+            res.status(200).send("Processing is Complete")
+        })
+        // conditional because ffmpeg runs Asynchronously 
+        .on("error", (err) =>{
+            console.log(`An error has occurred: ${err.message}`);
+            res.status(500).send(`We've run into an Error: ${err.message}`);
+        })
+        .save(outputFilePath);
+        
 })
 
+const port = process.env.PORT || 3000 //standard way to provide port at runtime
 app.listen(port, () =>{
     console.log(`VideoApp Listening at https://localhost:${port}`)
 })

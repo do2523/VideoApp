3cc6128fec131d3f659bcc0e5a0da941b7dad67e
do2523 <do262431@gmail.com>
2024-07-30 13:26:42 -0400

    Implement `deleteFile` function to delete a file asynchronously
    - Added `deleteFile` function that takes a file path as input and returns a Promise.
    - Checks if the file exists using `fs.existsSync()`.
    - If the file exists, attempts to delete it with `fs.unlink()` and resolves or rejects the Promise based on the result.
    - Logs appropriate messages for file deletion success, failure, or if the file does not exist.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # VideoApp
-# ---Currently working on: Docker file-----
--- video-processing-server/src/storage.ts
+++ video-processing-server/src/storage.ts
@@ -16,24 +16,12 @@ const localProcessedVideoPath = "./processed-videos";
 export function setupDirectories() {
 }
 
+
 /** 
  * @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}
- * @param processedVideoName - The name of the file to convert to {@link localProcessedVideoPath}
+ * @param processedVideoName - The name of the file to convert from {@link localProcessedVideoPath}
  * @returns - A promise that resolves when the video has been converted
 */
-
-export async function downloadRawVideo(fileName: string){
-    // await. Since this is an async function and we want to console.log last
-    await storage.bucket(rawVideoBucketName)
-        .file(fileName)
-        .download({ destination: `${localRawVideoPath}/${fileName}`})
-
-    console.log(`gs//${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}`)
-}
-
-export async function downloadProcessedVideo(fileName: string){
-
-}
 
 export function convertVideo(rawVideoName: string, processedVideoName: string) {
     // <void> so it dosen't have to resolve with a value
@@ -52,4 +40,68 @@ export function convertVideo(rawVideoName: string, processedVideoName: string) {
             .save(`${localProcessedVideoPath}/${processedVideoName}`);
     })
 
+}
+
+export async function downloadRawVideo(fileName: string){
+    // await. Since this is an async function and we want to console.log last
+    await storage.bucket(rawVideoBucketName)
+        .file(fileName)
+        .download({ destination: `${localRawVideoPath}/${fileName}`})
+
+    console.log(`gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}`)
+}
+
+export async function downloadProcessedVideo(fileName: string){
+    await storage.bucket(processedVideoBucketName)
+        .file(fileName)
+        .download({destination: `${localProcessedVideoPath}/${fileName}`})
+
+    console.log(`gs://${processedVideoBucketName}/${fileName} downloaded to ${localProcessedVideoPath}/${fileName}`)
+}
+
+
+/**
+ * @param fileName - The name of the file to upload from the >
+ * {@link localProcessedVideoPath} folder into the {@link processedVideoBucketName}
+ * @returns A promise that resolves when the file has been uploaded
+*/
+
+export async function uploadProcessedVideo(fileName: string){
+    const bucket = storage.bucket(processedVideoBucketName);
+    await bucket.upload(`${localProcessedVideoPath}/${fileName}`, {
+        destination: fileName
+    })
+
+    console.log(
+        `${localProcessedVideoPath}/${fileName} uploaded to gs://${processedVideoBucketName}/${fileName}`
+    );
+
+    await bucket.file(fileName).makePublic();
+}
+
+
+/**
+ * @param filePath - The path of the files to delete
+ * @returns A promise that resolves when the file has been deleted
+ */
+
+export function deleteFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        // Check if the file exists
+        if (fs.existsSync(filePath)) {
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.log(`Failed to delete files at ${filePath}`, err);
+                    reject(err);
+                } else{
+                    console.log(`File deleted at ${filePath}`);
+                    resolve()
+                }
+            })
+        }
+        else {
+            console.log(`File not found at, ${filePath}`) //check
+            resolve()
+        }
+    })
 }

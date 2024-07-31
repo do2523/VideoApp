0f85bb3f503d77af739d5b4a79a9da8f427308f9
do2523 <do262431@gmail.com>
2024-07-31 13:36:19 -0400

    feat: add endpoint to process video using Cloud Pub/Sub message
    - Added POST endpoint `/process-video` to handle video processing requests.
    - Extracted and decoded Cloud Pub/Sub message from the request body.
    - Parsed message data to retrieve bucket and filename.
    - Added error handling for missing or invalid filename in the message payload.

--- video-processing-server/src/index.ts
+++ video-processing-server/src/index.ts
@@ -1,25 +1,26 @@
 import express from "express";
 import ffmpeg from "fluent-ffmpeg";   // <symbolic link> Issue installing the actual ffmpeg  ***
-import { setupDirectories } from './storage.ts'
+import { setupDirectories } from './storage'
+
+setupDirectories();
 
 const app = express();
 app.use(express.json()); // So express can handle JS req
 
 app.post("/process-video", (req, res) =>{
-    // Get path of the input video file from the request body
-
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
-
-    if (!inputFilePath){
-        res.status(400).send("Bad Request: Missing Input File Path")
+    // Get bucket and filename from Cloud Pub/Sub message
+    let data;
+    try {
+        const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+        data = JSON.parse(message);
+        if (!data.name) {
+            throw new Error ('Invalid message payload recieved')
+        }
     }
-    if (!outputFilePath){
-        res.status(400).send("Bad Request: Missing Output File Path")
+    catch (error) {
+        console.error(error);
+        return res.status(400).send('Bad Request: missing filename');
     }
-
-    
-        
 })
 
 const port = process.env.PORT || 3000 //standard way to provide port at runtime

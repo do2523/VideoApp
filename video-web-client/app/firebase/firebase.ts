d9ad83776b2639e3aa362286b755d965f5c1a4f9
do2523 <do262431@gmail.com>
2024-08-11 13:51:09 -0400

    feat: Implement Firebase authentication with Google Sign-In
    - Added Firebase configuration using environment variables for secure management of sensitive data.
    - Initialized Firebase app with `initializeApp` using the config.
    - Set up Firebase Authentication with Google Sign-In using `signInWithPopup`.
    - Created helper functions for user sign-in, sign-out, and handling authentication state changes.

---
+++ video-web-client/app/firebase/firebase.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+// User Sign in through Google Popup
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+// User Sign out
+export function signOut() {
+    return auth.signOut();
+}
+
+// Function to unsubscribe callback
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback)
+}

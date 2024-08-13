1a0a446cfb9c566f6b147cade628196e3b2a37a5
do2523 <do262431@gmail.com>
2024-08-13 13:52:07 -0400

    feat: Add SignIn component with conditional auth buttons

--- video-web-client/app/navbar/navbar.tsx
+++ video-web-client/app/navbar/navbar.tsx
@@ -4,15 +4,32 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css"
 import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth"
 
-export default function Navbar() {
+function Navbar() {
+    // Init user state
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+        setUser(user);
+    })
+    
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+    }, []);
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
                 <Image width={350} height={100} 
                     src="/VideoWebAppLogo.png" alt="Video Web App Logo"/>
             </Link>
-            <SignIn />
+            <SignIn user={user} />
         </nav>
     );
 }
+
+export default Navbar;
--- video-web-client/app/navbar/sign-in.tsx
+++ video-web-client/app/navbar/sign-in.tsx
@@ -1,17 +1,29 @@
 import { Fragment } from 'react';
 import { signInWithGoogle, signOut } from '../firebase/firebase';
 import styles from './sign-in.module.css';
+import { User } from 'firebase/auth';
 
+interface SignInProps {
+    user: User | null;
+}
 
-export default function SignIn() {
+export default function SignIn({ user }: SignInProps) {
     return (
-        <Fragment>
-        <button className={styles.signin} onClick={signOut}>
-            Sign Out
-        </button>
-        <button className={styles.signin} onClick={signInWithGoogle}>
-            Sign In
-        </button>
-        </Fragment>
+        <div>
+            { user ?
+            (
+                <button className={styles.signin} onClick={signOut}>
+                Sign Out
+            </button>
+            ) : (
+                <button className={styles.signin} onClick={signInWithGoogle}>
+                Sign In
+            </button>
+            )
+
+            }
+
+
+        </div>
     )
 }

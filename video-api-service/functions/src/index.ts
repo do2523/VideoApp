d459db4840257d5e87a5d742e41b62f99826ba6b
do2523 <do262431@gmail.com>
2024-08-16 14:01:27 -0400

    feat: install Google Cloud Storage library and instantiate Storage instance

--- video-api-service/functions/src/index.ts
+++ video-api-service/functions/src/index.ts
@@ -2,10 +2,12 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {

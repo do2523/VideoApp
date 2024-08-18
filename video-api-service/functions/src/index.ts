86a40cef3c016ab01e142c5d2bf194d6b40b170a
do2523 <do262431@gmail.com>
2024-08-18 13:55:38 -0400

    chore: Modify generateUploadUrl function for secure video uploads
    - Implemented a Firebase function to generate signed URLs for video uploads to Google Cloud Storage.
    - Ensured user authentication before URL generation.
    - Set URLs to expire after 10 minutes to enhance security.
    - Updated permissions to allow the function to interact with the storage bucket and generate signed URLs.

--- video-api-service/functions/src/index.ts
+++ video-api-service/functions/src/index.ts
@@ -3,11 +3,14 @@ import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
 
 initializeApp();
 
 const firestore = new Firestore();
 const storage = new Storage();
+const rawVideoBucketName = "swe314-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -21,11 +24,28 @@ export const createUser = functions.auth.user().onCreate((user) => {
   return;
 });
 
-export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
-  // Check if the user is authentication
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) =>{
+  // Check if the user is authenticated
   if (!request.auth) {
     throw new functions.https.HttpsError(
       "failed-precondition",
-      "The function must be called while authenticated."
+      "The function must be called while authenticated"
     );
   }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading files
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 10 * 60 * 1000, // 10 Minutes
+  });
+
+  return {url, fileName};
+});
